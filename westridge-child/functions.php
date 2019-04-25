a3b75ab31a31c991413796f5d7d46572a55d9c03
Josh Espinoza <joshespi@gmial.com>
2019-04-25 16:23:35 -0600

    fixed the parent stylesheet enqueue so i could push a new version with the parent theme

--- westridge-child/functions.php
+++ westridge-child/functions.php
@@ -3,16 +3,9 @@
 Add stylesheets/javascripts to enqueue
 ============================================================================================*/
 function my_theme_enqueue_styles() {
-
-    $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
-
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css', '1.0.1', true );
+    wp_enqueue_style('parent-theme', get_template_directory_uri() .'/style.css');
     wp_enqueue_script( 'child_scripts', get_theme_file_uri().'/assets/js/child-scripts.js', '', '1.0', true);
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get('Version')
-    );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', '' , '1.5', false);
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 /*==========================================================================================
