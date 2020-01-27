175911de5851093d20526071b00b09a42377bd53
Josh Espinoza <joshespi@gmial.com>
2020-01-27 09:17:56 -0700

    changed ADA message to pull from a global page so that all sites can be controlled from one location

--- westridge-child/footer.php
+++ westridge-child/footer.php
@@ -13,7 +13,17 @@
 				 	
 				</ul>
 			</section>
-			<p>The Provo City School District supports equal opportunity for students, staff, parents and guests and complies with state and federal law regulation including Title VII of the Civil Rights Act and Title IX of the Education Amendments.  The District does not discriminate in programs services, activities or employment on the basis of religion, sex, sexual orientation, race, color, national origin, age, veteran or military status, or disability. Inquiries regarding students, programs, services or activity compliance may be directed to the Executive Director of Student Services at 280 West 940 North, Provo, UT 84604 801-374-4814. For program, service or activity accommodations under the ADA, please contact Student Services at 801-374-4838 or donnan@provo.edu. Inquiries regarding employment compliance may be directed to the Executive Director of Human Resources at 280 West 940 North, Provo, UT 84604 801-374-4938. For applicant or employee accommodations under the ADA, please contact the Benefit Coordinator 801-374-4847 or rebeccavo@provo.edu.</p>
+			<?php
+		// create a new cURL resource
+		$globalfooter = curl_init();
+		// set URL and other appropriate options
+		curl_setopt($globalfooter, CURLOPT_URL, 'https://globalassets.provo.edu/globalpages/ada-footer.php');
+		curl_setopt($globalfooter, CURLOPT_HEADER, 0);
+		// grab URL and pass it to the browser
+		curl_exec($globalfooter);
+		// close cURL resource, and free up system resources
+		curl_close($globalfooter);
+		?>
 			<p class="feedback"><a href="<?php echo get_home_url(); ?>/website-feedback/">Help us improve our website. Submit your comments and suggestions through our Website Feedback Form.</a></p>
 		</footer>
 		<?php wp_footer(); ?>
